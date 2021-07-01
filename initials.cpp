a03570bf45e9b9c83631943213c216cfc93461d5
Junglemouse10 <86082975+Junglemouse10@users.noreply.github.com>
2021-07-01 12:01:36 -0700

    Add files via upload

---
+++ initials.cpp
@@ -0,0 +1,13 @@
+// g++ initials.cpp -o lv.o
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << " LL     V      V    LL  \n";
+	cout << "L    L  V V V    L   L  \n";
+	cout << "L         V V     L     \n";
+	cout << "L  L              V   V \n";
+	cout << " VV                  VV \n";
+	
+	return 0;
+}
